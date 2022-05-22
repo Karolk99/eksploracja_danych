2d4cd49d28a4049110d77e64836934c850af6b97
Karolk99 <konkolisz@gmail.com>
2022-05-22 13:12:34 +0200

    Repair data filters

--- filters/chunk_filter.py
+++ filters/chunk_filter.py
@@ -1,36 +1,47 @@
-from abc import ABC, abstractclassmethod, abstractmethod
+from abc import ABC, abstractmethod
 
 from typing import List
 import pandas as pd
 
+
 class AbstractFilter(ABC):
-    
     @abstractmethod
     def filter(self, data: pd.DataFrame) -> pd.DataFrame:
         pass
 
+
 class DropColumnsFilter(AbstractFilter):
     def init(self, columns_to_drop: List[str]):
         self.columns_to_drop = columns_to_drop
-    
+
     def filter(self, data: pd.DataFrame) -> pd.DataFrame:
         return data.drop(
             columns=self.columns_to_drops,
         )
 
+
 class SpecificValuesInRowFilter(AbstractFilter):
     def init(self, column: str, acceptable_values: List):
         self.column = column
         self.acceptable_values = acceptable_values
-    
+
     def filter(self, data: pd.DataFrame) -> pd.DataFrame:
         return data[data[self.column].isin(self.acceptable_values)]
+
 
 class DateFilter(AbstractFilter):
     def init(self, after: pd.Timestamp, before: pd.Timestamp, column: str):
         self.after = after
         self.before = self.before
         self.column = column
-    
+
     def filter(self, data: pd.DataFrame) -> pd.DataFrame:
-        return data.loc[self.before:self.after]
+        return data.loc[self.before : self.after]
+
+
+class DistinctValuesFilter(AbstractFilter):
+    def init(self, distinct_columns: List):
+        self.distinct_columns = distinct_columns
+
+    def filter(self, data: pd.DataFrame) -> pd.DataFrame:
+        return data.drop_duplicates(subset=self.distinct_columns)
---
+++ filters/large_data_fitler.py
@@ -0,0 +1,54 @@
+from typing import List
+import pandas as pd
+from chunk_filter import AbstractFilter
+
+
+class LargeDataFilter:
+    memory_available: int = 1000000000
+
+    def filter_data(
+        self,
+        input_file: str,
+        output_file: str,
+        filter: AbstractFilter,
+    ) -> None:
+        csv_iterator = self._create_csv_iterator(input_file)
+
+        first_iteration = True
+        for chunk in csv_iterator:
+            filter.filter(chunk).to_csv(
+                output_file, mode="a", header=first_iteration, index=False
+            )
+            first_iteration = False if first_iteration else first_iteration
+
+    def filter_distinct(
+        self,
+        input_file: str,
+        output_file: str,
+        filter: AbstractFilter,
+    ) -> None:
+        csv_iterator = self._create_csv_iterator(input_file)
+
+        df = pd.concat([filter.filter(chunk) for chunk in csv_iterator])
+        df = filter.filter(df)
+
+        df.to_csv(output_file, mode="a", header=True, index=False)
+
+    def _create_csv_iterator(self, input_file):
+        chunk_size = self._calculate_chunk_size(input_file)
+
+        return pd.read_csv(
+            input_file,
+            iterator=True,
+            chunksize=int(chunk_size),
+        )
+
+    def _calculate_chunk_size(self, input_file):
+        nrows = 10
+        df_sample = pd.read_csv(
+            input_file,
+            nrows=nrows,
+        )
+
+        df_sample_size = df_sample.memory_usage(index=True).sum()
+        return (self.memory_available / df_sample_size) / nrows
